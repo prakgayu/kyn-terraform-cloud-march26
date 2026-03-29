a598be6dc3f3f83087b205caa34a7f4604478821
prakgayu <prakasamg@gmail.com>
2026-03-29 07:17:03 +0000

    change the env to dev to t2micro

--- c1-ec2-templates.tf
+++ c1-ec2-templates.tf
@@ -1,7 +1,8 @@
- resource "aws_launch_template" "ec2_instance" {
+resource "aws_launch_template" "ec2_instance" {
   name_prefix   = "prakasam-web-launch-template"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type["dev"]
+  update_default_version = true #we are upding the default version
 
   key_name               = "prakasam"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
@@ -10,7 +11,7 @@
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Hello from Auto Scaling" > /var/www/html/index.html
+echo "Hello from Auto Scaling prakasam $HOSTNAME" > /var/www/html/index.html
 EOF
 )
 }
--- c2.autoscaling.tf
+++ c2.autoscaling.tf
@@ -8,9 +8,20 @@ resource "aws_autoscaling_group" "web" {
   target_group_arns = [ aws_lb_target_group.app_tg.arn ]
   health_check_type = "ELB" #this is the health check type for the autoscaling group
     launch_template {
-        id      = aws_launch_template.ec2_instance.id
-        version = "2"
+        id      = aws_launch_template.ec2_instance.id 
+        version = aws_launch_template.ec2_instance.latest_version
     }
+
+  instance_refresh {
+    strategy = "Rolling"
+
+    preferences {
+      min_healthy_percentage = 50
+    }
+
+    triggers = ["launch_template"]
+  }
+
    tag {
      key                 = "Name"
      value               = "prakasam-autoscaling-group"
